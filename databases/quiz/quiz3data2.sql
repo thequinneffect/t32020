bec2f137671634945eaf3796cb3b14e6d88f927f
thequinneffect@desktop <n_quinny12@hotmail.com>
2020-10-09 22:24:45 +1100

    last q

---
+++ databases/quiz/quiz3data2.sql
@@ -0,0 +1,14 @@
+insert into table Student values ('1234567', 'Harry Roo', 21);
+insert into table Student values ('2345678', 'Chen Zhang', 25);
+insert into table Student values ('3456789', 'Patricia Li', 18);
+
+insert into table class values ('3311', 'Databases for fun and profit');
+insert into table class values ('6441', 'Security learning');
+insert into table class values ('1521', 'Deep dive into MIPS');
+
+insert into table enrolment values ('1234567', '3311', NULL);
+insert into table enrolment values ('2345678', '3311', 76);
+insert into table enrolment values ('2345678', '1521', 77);
+insert into table enrolment values ('2345678', '6441', 77);
+insert into table enrolment values ('1234567', '1521', NULL);
+
