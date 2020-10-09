bdcf71a6ce1da2e51a45b179a64a3a3615bf2b6f
thequinneffect <n_quinny12@hotmail.com>
2020-10-09 16:17:38 +1100

    testing updated schema

--- databases/pracs/3/schema.sql
+++ databases/pracs/3/schema.sql
@@ -1,26 +1,38 @@
 -- COMP3311 Prac 03 Exercise
 -- Schema for simple company database
 
+create domain depid as char(3) check (val ~ '[0-9]{3}');
+
 create table Employees (
-	tfn         char(11),
-	givenName   varchar(30),
+	tfn         char(11) check (tfn ~ '[0-9]{3}-[0-9]{3}-[0-9]{3}'),
+	givenName   varchar(30) not null,
 	familyName  varchar(30),
-	hoursPweek  float
+	hoursPweek  float check (hoursPweek >= 0 and
+								hoursPweek <= 168),
+	primary key (tfn)
 );
 
 create table Departments (
-	id          char(3),
-	name        varchar(100),
-	manager     char(11)
+	id          depid,
+	name        varchar(100) unique,
+	manager     char(11) unique not null,
+	primary key (id),
+	foreign key (manager) references (Employees.tfn)
 );
 
 create table DeptMissions (
-	department  char(3),
-	keyword     varchar(20)
+	department  depid,
+	keyword     varchar(20),
+	primary key (department, keyword)
+	foreign key (department) references (Departments.id)
 );
 
 create table WorksFor (
 	employee    char(11),
-	department  char(3),
-	percentage  float
+	department  depid,
+	percentage  float check (percentage > 0.0 and
+								percentage <= 1.0),
+	primary key (employee, department),
+	foreign key (employee) references (Employees.tfn),
+	foreign key (department) references (Departments.id)
 );
