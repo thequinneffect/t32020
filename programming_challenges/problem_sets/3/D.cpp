95e61abe5938065c15bc944fede1e7d133ae79b9
thequinneffect <n_quinny12@hotmail.com>
2020-10-11 17:23:58 +1100

    attempted, failed, but can reuse

--- programming_challenges/problem_sets/3/D.cpp
+++ programming_challenges/problem_sets/3/D.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <map>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -13,9 +15,36 @@ bool can_do(int cost) {
     int running_cost = 0;
     for (int i=1; i < n; i++) {
         if (s[i] == prev_char) continue;
-        pairs[{s[i], prev_char}]++;
-        if (running_cost > cost) return false;
+        char c1, c2;
+        c1 = (s[i] < prev_char) ? s[i] : prev_char;
+        c2 = (c1 == prev_char) ? s[i] : prev_char;
+        pairs[{c1,c2}] += 1;
         prev_char = s[i];
+    }
+    cout << "showing keyboard requirements for " << cost << ";\n";
+    for (auto p : pairs) {
+        cout << "(" << p.first.first << "," << p.first.second << ")" << " has num occurences = " << p.second << endl; 
+    }
+
+    // get the occurences
+    int num_pairs = pairs.size();
+    vector<int> counts(num_pairs);
+    int index = 0;
+    for (const auto &p : pairs) {
+        counts[index++] = p.second;
+    }
+    sort(counts.begin(), counts.end(), greater<int>());
+
+    // can have m-1 direct pairs, m-2 off by 1 pairs, m-3 off by 2 pairs etc.
+    int batch = m-1;
+    int total_cost = 0;
+    for (int i=0, b=0; i < num_pairs; i++, b++) {
+        if (b == batch) {
+            batch--;
+            b=0;
+        }
+        total_cost += counts[i] * (m-batch);
+        if (total_cost > cost) return false;
     }
     return true;
 }
@@ -27,9 +56,13 @@ int main() {
     int l = -1; //invariant: f(l) is false
     int r = (m-1)*(n-1)+1; //invariant: f(r) is true
     while (l < r-1) {
-        int m = (l+r)/2;
-        if (can_do(m)) r = m;
-        else l = m;
+        int mid = (l+r)/2;
+        if (can_do(mid)) {
+            r = mid;
+        }
+        else {
+            l = mid;
+        } 
     }
-    return r;
+    cout << r << endl;
 }
