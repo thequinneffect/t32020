e0d8dee8d3a2de766c1c929e28381b410e302df9
thequinneffect <n_quinny12@hotmail.com>
2020-12-03 10:53:50 +1100

    q1 done

--- databases/final/work/q1/q1.sql
+++ databases/final/work/q1/q1.sql
@@ -3,8 +3,22 @@
 
 -- ... helper views (if any) go here ...
 
+create or replace view longest_album_length(s_length)
+as
+select sum(s.length)
+from songs s
+group by s.on_album
+order by sum(s.length) desc
+limit 1
+;
+
 create or replace view q1("group",album,year)
 as
-... put your SQL here...
+select g.name, a.title, a.year
+from albums a
+join groups g on (g.id = a.made_by)
+join songs s on (s.on_album = a.id)
+group by a.id, g.name, a.title, a.year
+having sum(s.length) >= (select * from longest_album_length);
 ;
 
