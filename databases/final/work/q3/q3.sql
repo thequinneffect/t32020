248f249a78cda3bb2822e662f8e65d3f6af861d3
thequinneffect <n_quinny12@hotmail.com>
2020-12-03 12:23:02 +1100

    possibly done q4

--- databases/final/work/q3/q3.sql
+++ databases/final/work/q3/q3.sql
@@ -3,6 +3,27 @@
 
 -- ... helper views (if any) go here ...
 
+create or replace view num_guitars(nguitars)
+as
+select count(distinct p.instrument)
+from playson p
+where p.instrument ilike '%guitar%'
+;
+
+create or replace view num_instruments(ninstruments)
+as
+select count(distinct p.instrument) - (select * from num_guitars) + 1
+from playson p
+where p.instrument <> 'vocals';
+;
+
+create or replace view all_instruments(instrument)
+as
+select distinct p.instrument 
+from playson p
+where p.instrument <> 'vocals';
+;
+
 create or replace view q3(performer,ninstruments)
 as
 ...put your SQL here...
--- databases/final/work/q4/q4.sql
+++ databases/final/work/q4/q4.sql
@@ -6,10 +6,38 @@
 drop function if exists q4();
 drop type if exists SongCounts;
 create type SongCounts as ( "group" text, nshort integer, nlong integer );
+drop type if exists SongInfo;
+create type SongInfo as (title text, length integer);
 
 create or replace function
 	q4() returns setof SongCounts
 as $$
-... PLpgSQL code goes here ...
+declare
+	_group groups%rowtype;
+	_songinfo SongInfo;
+	_info SongCounts;
+begin
+	for _group in 
+		select * 
+		from groups g
+	loop 
+		_info.group := _group.name;
+		_info.nshort := 0;
+		_info.nlong := 0;
+		for _songinfo in 
+			select distinct s.title, s.length
+			from songs s
+			join albums a on (a.made_by = _group.id)
+			where s.on_album = a.id
+		loop
+			if (_songinfo.length < 180) then
+				_info.nshort := _info.nshort + 1;
+			elsif (_songinfo.length > 360) then
+				_info.nlong := _info.nlong + 1;
+			end if;
+		end loop;
+		return next _info;
+	end loop;
+end;
 $$ language plpgsql
 ;
