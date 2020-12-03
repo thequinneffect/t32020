fac44c242ece57dee3ac7a86e9033bdd1b936e78
thequinneffect <n_quinny12@hotmail.com>
2020-12-03 14:40:09 +1100

    possibly done q7

--- databases/final/work/q7/q7.sql
+++ databases/final/work/q7/q7.sql
@@ -1,3 +1,26 @@
 -- COMP3311 20T3 Final Exam Q7
 -- Put any helper views or PLpgSQL functions here
 -- You can leave it empty, but you must submit it
+
+drop function if exists instrumentsOf(_performer integer, _song integer);
+
+create or replace function
+    instrumentsOf(_performer integer, _song integer) returns text
+as $$
+declare
+    _instrument text;
+    _all text := '';
+begin
+    for _instrument in 
+        select distinct p.instrument 
+        from playson p
+        where p.song = _song and p.performer = _performer
+        order by p.instrument
+    loop
+        _all := _all || _instrument || ',';
+    end loop;
+    _all := rtrim(_all, ',');
+    return _all;
+end;
+$$ language plpgsql
+;
