69af2c47cc27fff0c4669e6ae251989e3503097d
thequinneffect <n_quinny12@hotmail.com>
2020-12-03 12:45:15 +1100

    possibly done 5

--- databases/final/work/q5/q5.sql
+++ databases/final/work/q5/q5.sql
@@ -11,7 +11,30 @@ create type GroupGenres as ("group" text, genres text);
 create or replace function
     q5() returns setof GroupGenres
 as $$
-... PLpgSQL code goes here ...
+declare
+    _group groups%rowtype;
+    _info GroupGenres;
+    _genre text;
+begin
+    for _group in 
+		select * 
+		from groups g
+	loop 
+        _info.group = _group.name;
+        _info.genres := '';
+        for _genre in 
+            select distinct a.genre
+            from albums a
+            where (a.made_by = _group.id)
+            order by a.genre
+        loop 
+            _info.genres := _info.genres || _genre || ',';
+        end loop;
+        --_info.genres := substring(info.genres, 1, char_length(info.genres) - 1);
+        _info.genres := rtrim(_info.genres, ',');
+        return next _info;
+    end loop;
+end;
 $$ language plpgsql
 ;
 
