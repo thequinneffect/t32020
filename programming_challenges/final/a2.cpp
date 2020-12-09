398e7d2f74685d5f8335a65f10a445d24f29e9a1
thequinneffect <n_quinny12@hotmail.com>
2020-12-09 15:36:19 +1100

    faster

--- programming_challenges/final/a2.cpp
+++ programming_challenges/final/a2.cpp
@@ -9,10 +9,10 @@ using namespace std;
 typedef long long ll;
 
 ll n;
-vector<ll> k;
+vector<ll> k, rhs;
 
 vector<ll> rot(vector<ll> a) {
-    printf("doing rot\n");
+    //printf("doing rot\n");
     vector<ll> res;
     for (int i=0; i < 5; i++) {
         res.push_back(a[i]);
@@ -23,23 +23,13 @@ vector<ll> rot(vector<ll> a) {
     return res;
 }
 
-vector<ll> mul(vector<ll> a, vector<ll> b) {
-    printf("doing mul\n");
+vector<ll> mul(vector<ll>& a) {
+    //printf("doing mul\n");
     vector<ll> res;
     for (int i=0; i < 5; i++) {
-        res.push_back((a[i] * b[i]) % mod);
+        res.push_back((a[i] * rhs[i]) % mod);
     }
     return res;
-}
-
-vector<ll> combine(vector<ll> a, vector<ll> b) {
-    printf("doing combine\n");
-    return mul(a, rot(b));
-}
-
-vector<ll> combine_n(vector<ll> k, int n) {
-    if (n==0) return k;
-    return combine(combine_n(k, n-1), k);
 }
 
 int main() {
@@ -50,13 +40,18 @@ int main() {
         cin >> temp;
         k.push_back(temp);
     }
+    rhs = rot(k);
     if (debug) {
         for (int i=0; i < 5; i++) {
             printf("%lld ", k[i]);
         }
         printf("\n");
     }
-    auto res = combine_n(k, n);
+
+    vector<ll> res = k;
+    for (int i=0; i < n; i++) {
+        res = mul(res);
+    }
     for (int i : res) {
         printf("%d ", i);
     }
